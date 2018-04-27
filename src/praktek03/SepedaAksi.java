fa19a84b198b123fe683b96de153830da0a7c10a
frida <frida.larasati@gmail.com>
2018-04-27 08:57:21 +0800

    Menggunakan method get

--- src/praktek03/SepedaAksi.java
+++ src/praktek03/SepedaAksi.java
@@ -6,6 +6,14 @@ public class SepedaAksi {
         s.setMerk("Butterfly");
         s.setWarna("Merah");
         s.setHarga(1000);
+        
         s.cetakInfo();
+        
+        System.out.print("Merknya \t: ");
+        System.out.println(s.getMerk());
+        System.out.print("Warnanya \t: ");
+        System.out.println(s.getWarna());
+        System.out.print("Harganya \t: ");
+        System.out.println(s.getHarga());
     }
 }
